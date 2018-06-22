ad7c4f9e22b6061ecca8a57ba902b04052b67838
Yuva <yuva@codemancers.com>
2018-06-22 11:50:25 +0530

    add more icons to the mix!

--- apps/dockup_ui/web/static/js/components/deployment_status.js
+++ apps/dockup_ui/web/static/js/components/deployment_status.js
@@ -1,21 +1,22 @@
 import React from 'react';
 
+const statusToIcon = {
+  "queued": "icon-queued",
+  "processing": "icon-sync",
+  "cloning_repo": "icon-cloning-repo",
+  "starting": "icon-sync",
+  "checking_urls": "icon-sync",
+  "started": "icon-deployed",
+  "hibernating_deployment": "icon-sync",
+  "deployment_hibernated": "icon-hibernated",
+  "waking_up_deployment": "icon-sync",
+  "deleting_deployment": "icon-sync",
+  "deployment_deleted": "icon-deleted",
+  "deployment_failed": "icon-errored"
+}
+
 const DeploymentStatus = ({status}) => {
-  let icon = "icon-sync";
-  switch (status) {
-    case 'started':
-      icon = "icon-deployed";
-      break;
-    case 'hibernating_deployment':
-      icon = "icon-deployed";
-      break;
-    case 'deployment_hibernated':
-      icon = "icon-deployed";
-      break;
-    case 'deployment_deleted':
-      icon = "icon-deleted";
-      break;
-    }
+  let icon = statusToIcon[status] || "icon-sync";
 
   return(
     <div className="icon" title={status}>
